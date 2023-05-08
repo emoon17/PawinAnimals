1541d68d5370e4db40fa17ebf4c1122a22bf675c
eunhye <eunhye>
2023-05-08 20:21:03 +0900

    TestUnit 예제

--- src/test/java/com/pawin/PawinApplicationTests.java
+++ src/test/java/com/pawin/PawinApplicationTests.java
@@ -1,13 +1,31 @@
 package com.pawin;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PawinApplicationTests {
 
-	@Test
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	//@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	void 더하기 () {
+		
+		logger.info("############더하기 테스트 #####");
+		int a = 10;
+		int b = 20;
+		assertEquals(30, sum(a,b));
+	}
 
+	int sum (int x, int y) {
+		return x + y;
+	}
 }
